ad26f0578abdf0faa3b8e20ea92562638971e228
seifeddine10 <49524346+seifeddine10@users.noreply.github.com>
2021-03-03 01:13:34 +0100

    Squelette Projet PiDev

---
+++ The_Crew/src/utils/Connexion.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+/**
+ *
+ * @author SeifBS
+ */
+public class Connexion {
+    final static String URL="jdbc:mysql://localhost:3306/aura";
+    final static String LOGIN="root";
+    final static String PWD="";
+    static Connexion instance=null;
+    private Connection cnx;
+    private Connexion(){
+    try {
+    cnx=DriverManager.getConnection(URL,LOGIN,PWD);
+    System.out.println("Ma Connexion est etablie "); 
+   }
+    catch (SQLException ex){
+   
+   System.out.println("Pas De Connexion");
+   }
+    
+    
+    }
+    
+public static Connexion getInstance(){
+if(instance==null){
+
+instance=new Connexion();
+}
+return instance ;
+}
+public Connection getConnection(){
+return cnx;
+}
+
+}
+    
+
