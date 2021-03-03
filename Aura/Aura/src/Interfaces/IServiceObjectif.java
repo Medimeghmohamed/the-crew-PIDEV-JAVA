8b5b25a552abcf12bb65e0cae950eb2ddc15ac98
Chirine Nasri <47995210+chirinenasri@users.noreply.github.com>
2021-03-03 02:04:56 +0100

    Add files via upload

--- Aura/Aura/src/Interfaces/IServiceObjectif.java
+++ Aura/Aura/src/Interfaces/IServiceObjectif.java
@@ -6,13 +6,16 @@
 package Interfaces;
 import Entities.Objectif;
 import java.util.List;
+import javafx.collections.ObservableList;
 /**
  *
  * @author Chirine
  */
 public interface IServiceObjectif {
     public void ajouterObjectif(Objectif o);
-    public List<Objectif> afficherObjectifs();
+    public ObservableList<String> getMesObjectifs(int id);
+    public String getIdObj(String obj, int id);
+    public ObservableList<Objectif> afficherObjectifs();
     public void modifierObjectif(Objectif o);
     public void supprimerObjectif(String id);
     
