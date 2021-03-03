8b55431a3e8ac5983fb45152f43a3c8b700e2b5f
Chirine Nasri <47995210+chirinenasri@users.noreply.github.com>
2021-03-03 21:11:21 +0100

    Add files via upload

--- The_Crew/src/Services/ServiceObjectif.java
+++ The_Crew/src/Services/ServiceObjectif.java
@@ -81,7 +81,7 @@ public class ServiceObjectif implements IServiceObjectif {
         try {
             Statement st = cnx.createStatement();
             //nb: on ne peut pas modifier la date
-            String query = "UPDATE  objectif SET description  = '" + o.getDescription() + "', reponse = '" + o.getReponse() + ", duree = " + o.getDuree() + " WHERE id = " + o.getId() + "";
+            String query = "UPDATE  objectif SET description  = '" + o.getDescription() + "', reponse = '" + o.getReponse() + "', duree = '" + o.getDuree() + "' WHERE id = '" + o.getId() + "'";
             st.executeUpdate(query);
             System.out.println("modification avec succes");
         } catch (SQLException ex) {
@@ -94,7 +94,7 @@ public class ServiceObjectif implements IServiceObjectif {
     public void supprimerObjectif(String id) {
         try {
             Statement st = cnx.createStatement();
-            String query = "DELETE FROM `objectif` WHERE id = " + id + "";
+            String query = "DELETE FROM `objectif` WHERE id = '" + id + "'";
             st.executeUpdate(query);
             System.out.println("suppression avec succes");
         } catch (SQLException ex) {
@@ -110,7 +110,7 @@ public class ServiceObjectif implements IServiceObjectif {
             Statement st = cnx.createStatement();
             String query = "select `description` FROM `objectif` WHERE idClient='" + id + "'";
             ResultSet rs = st.executeQuery(query);
-            while (rs.next()) {   
+            while (rs.next()) {
                 mesObjectifs.add(rs.getString("description"));
             }
 
@@ -123,20 +123,143 @@ public class ServiceObjectif implements IServiceObjectif {
 
     @Override
     public String getIdObj(String obj, int idd) {
-        String id="";
+        String id = "";
         try {
             Statement st = cnx.createStatement();
             String query = "select `id` FROM `objectif` WHERE description='" + obj + "' AND idClient='" + idd + "'";
             ResultSet rs = st.executeQuery(query);
-            while (rs.next()) {   
+            while (rs.next()) {
                 id = rs.getString("id");
             }
-            
+
         } catch (SQLException ex) {
             System.out.println("erreur get IdOBJ pour suivi");
             System.out.println(ex);
         }
         return id;
+    }
+
+    //stats
+    @Override
+    public int getRepObj(String idObj) {
+        int rep = 0;
+        try {
+            Statement st = cnx.createStatement();
+            String query = "select `reponse` FROM `objectif` WHERE id='" + idObj + "'";
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                rep = rs.getInt("reponse");
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("erreur getRepObj pour suivi (stats)");
+            System.out.println(ex);
+        }
+        return rep;
+    }
+
+    @Override
+    public ObservableList<Objectif> trierObjectifparRep() {
+        ObservableList<Objectif> objectifs = FXCollections.observableArrayList();
+        try {
+            Statement st = cnx.createStatement();
+            String query = "select * from `objectif` ORDER BY reponse";
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                Objectif o = new Objectif();
+                o.setId(rs.getString(1));
+                o.setDescription(rs.getString(2));
+                o.setReponse(rs.getInt(3));
+                o.setDate(rs.getString(4));
+                //o.setDate(rs.getTimestamp(4).toString());
+                o.setDuree(rs.getInt(5));
+                o.setIdCli(rs.getString(6));
+                objectifs.add(o);
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("erreur trier objectifs par reponse objectifs");
+            System.out.println(ex);
+        }
+        return objectifs;
+    }
+
+    @Override
+    public ObservableList<Objectif> trierObjectifparId() {
+        ObservableList<Objectif> objectifs = FXCollections.observableArrayList();
+        try {
+            Statement st = cnx.createStatement();
+            String query = "select * from `objectif` ORDER BY id";
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                Objectif o = new Objectif();
+                o.setId(rs.getString(1));
+                o.setDescription(rs.getString(2));
+                o.setReponse(rs.getInt(3));
+                o.setDate(rs.getString(4));
+                //o.setDate(rs.getTimestamp(4).toString());
+                o.setDuree(rs.getInt(5));
+                o.setIdCli(rs.getString(6));
+                objectifs.add(o);
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("erreur trier objectifs par id objectifs");
+            System.out.println(ex);
+        }
+        return objectifs;
+    }
+
+    @Override
+    public ObservableList<Objectif> trierObjectifparDesc() {
+        ObservableList<Objectif> objectifs = FXCollections.observableArrayList();
+        try {
+            Statement st = cnx.createStatement();
+            String query = "select * from `objectif` ORDER BY description";
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                Objectif o = new Objectif();
+                o.setId(rs.getString(1));
+                o.setDescription(rs.getString(2));
+                o.setReponse(rs.getInt(3));
+                o.setDate(rs.getString(4));
+                //o.setDate(rs.getTimestamp(4).toString());
+                o.setDuree(rs.getInt(5));
+                o.setIdCli(rs.getString(6));
+                objectifs.add(o);
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("erreur trier objectifs par description objectifs");
+            System.out.println(ex);
+        }
+        return objectifs;
+    }
+
+    @Override
+    public ObservableList<Objectif> rechercherObjectif(String s) {
+        ObservableList<Objectif> objectifs = FXCollections.observableArrayList();
+        try {
+            Statement st = cnx.createStatement();
+            String query = "select * from `objectif` WHERE id LIKE '%" + s + "%' OR description LIKE '%" + s + "%' OR reponse LIKE '%" + s + "%' OR dateDebut LIKE '%" + s + "%' OR duree LIKE '%" + s + "%'";
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                Objectif o = new Objectif();
+                o.setId(rs.getString(1));
+                o.setDescription(rs.getString(2));
+                o.setReponse(rs.getInt(3));
+                o.setDate(rs.getString(4));
+                //o.setDate(rs.getTimestamp(4).toString());
+                o.setDuree(rs.getInt(5));
+                o.setIdCli(rs.getString(6));
+                objectifs.add(o);
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("erreur rechercher objectif");
+            System.out.println(ex);
+        }
+        return objectifs;
     }
 
 }
