acecfb579a25b0a5995d7680b76ef34001a2904f
nourdekhil <47943718+nourdekhil@users.noreply.github.com>
2021-03-04 01:09:06 +0100

    Add files via upload

---
+++ The_Crew/src/Services/ServiceContact.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Service;
+
+import Entities.Contact;
+import Services.IServiceContact;
+import java.sql.Connection;
+import Utils.Maconnexion;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author Nour Dekhil
+ */
+public class ServiceContact implements IServiceContact {
+
+    Connection cnx;
+
+    public ServiceContact() {
+        cnx = Maconnexion.getInstance().getCnx();
+
+    }
+
+    @Override
+    public void ajouterContact(Contact c) {
+
+        try {
+
+            Statement stm = cnx.createStatement();
+            String query = "INSERT INTO `contact`( `id`,`type`, `id_client`, `id_coach`, `champ`, `date`,`etat`)"
+                    + "VALUES ('"
+                    + c.getId() + "','"
+                    + c.getType() + "','"
+                    + c.getId_client() + "','"
+                    + c.getId_coach() + "','"
+                    + c.getChamp() + "','"
+                    + c.getDate() + "','"
+                    + "non verif" + "')";
+
+            stm.executeUpdate(query);
+            System.out.println("ajout avec succes");
+        } catch (SQLException ex) {
+            System.out.println("erreur ajout");
+            System.out.println(ex);
+        }
+
+    }
+
+    @Override
+    public ObservableList<Contact> AfficherContact() {
+        ObservableList<Contact> contacts = FXCollections.observableArrayList();
+        try {
+            Statement stm = cnx.createStatement();
+            String query = "select * from `contact`";
+
+            ResultSet rst = stm.executeQuery(query);
+
+            while (rst.next()) {
+                Contact c = new Contact();
+
+                c.setId(rst.getInt(1));
+                c.setType(rst.getString(2));
+                c.setId_client(rst.getString(3));
+                c.setId_coach(rst.getString(4));
+                c.setChamp(rst.getString(5));
+                c.setDate(rst.getString(6));
+                c.setEtat(rst.getString(7));
+                //c.setDate(rs.getTimestamp(4).toString());
+                contacts.add(c);
+
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("erreur affichage ");
+            System.out.println(ex);
+        }
+
+        return contacts;
+
+    }
+
+    @Override
+    public void SupprimerContact(String id) {
+        int Id = Integer.parseInt(id);
+        try {
+            Statement st = cnx.createStatement();
+            String query = "DELETE FROM contact WHERE id = " + id + "";
+            st.executeUpdate(query);
+            System.out.println("suppression avec succes");
+        } catch (SQLException ex) {
+            System.out.println("erreur supprimer challenge");
+            System.out.println(ex);
+        }
+    }
+
+    /*public void ModifierContact(Contact c) {
+        try {
+            Statement stm = cnx.createStatement();
+
+            String query = "UPDATE  contact SET etat  = '" + c.getEtat() + " WHERE id = " + c.getId() + "";
+            stm.executeUpdate(query);
+            System.out.println(" succes");
+        } catch (SQLException ex) {
+            System.out.println("erreur ");
+            System.out.println(ex);
+        }
+    }*/
+    @Override
+    public void ModifierContact(String id, String etat) {
+        try {
+            Statement stm = cnx.createStatement();
+
+            String query = "UPDATE  contact SET etat= "+etat+" WHERE id ="+ id+"";
+            
+           
+            
+            
+            stm.executeUpdate(query);
+            System.out.println(" succes");
+        } catch (SQLException ex) {
+            System.out.println("erreur ");
+            System.out.println(ex);
+        }
+    }
+
+}
