a77a45e5df0762429a1426b3c40de87d1e7d6d94
Medimeghmohamed <48247888+Medimeghmohamed@users.noreply.github.com>
2021-03-10 18:02:50 +0100

    Add files via upload

--- aura.sql
+++ aura.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  sam. 27 fév. 2021 à 22:14
--- Version du serveur :  5.7.26
--- Version de PHP :  7.3.5
+-- Généré le : mer. 10 mars 2021 à 17:01
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `aura`
+-- Base de données : `aura`
 --
 
 -- --------------------------------------------------------
@@ -32,39 +31,25 @@ DROP TABLE IF EXISTS `activite`;
 CREATE TABLE IF NOT EXISTS `activite` (
   `id` int(16) NOT NULL,
   `idcoach` varchar(255) NOT NULL,
-  `idclient` varchar(255) NOT NULL,
   `duree` varchar(255) NOT NULL,
-  `date` date NOT NULL,
+  `date` varchar(255) NOT NULL,
   `nombremax` int(16) NOT NULL,
   `type` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `lieu` varchar(255) NOT NULL,
+  `nombre_parti` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `act1` (`idcoach`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Structure de la table `admin`
+-- Déchargement des données de la table `activite`
 --
 
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE IF NOT EXISTS `admin` (
-  `id` varchar(255) NOT NULL,
-  `nom` varchar(255) NOT NULL,
-  `prenom` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `tel` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `admin`
---
-
-INSERT INTO `admin` (`id`, `nom`, `prenom`, `email`, `tel`) VALUES
-('12', 'Ahmed', 'ou', 'aa', 'aa');
+INSERT INTO `activite` (`id`, `idcoach`, `duree`, `date`, `nombremax`, `type`, `description`, `lieu`, `nombre_parti`) VALUES
+(125, '12', '1', '02/04/2021', 21, '212', '21', '21', 0),
+(156, '12', '12', '17/03/2021', 100, '21', '21', '211', 4),
+(1256, '12', '1', '02/04/2021', 21, '212', '21', '21', 0);
 
 -- --------------------------------------------------------
 
@@ -79,12 +64,10 @@ CREATE TABLE IF NOT EXISTS `article` (
   `theme` varchar(255) NOT NULL,
   `nom_auteur` varchar(255) NOT NULL,
   `date` date NOT NULL,
-  `article` longtext NOT NULL,
-  `id_coach` varchar(255) NOT NULL,
-  `id_client` varchar(255) NOT NULL,
+  `article` varchar(255) NOT NULL,
+  `id_user` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `art1` (`id_client`),
-  KEY `art2` (`id_coach`)
+  KEY `art2` (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -112,55 +95,18 @@ CREATE TABLE IF NOT EXISTS `challenge` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `client`
---
-
-DROP TABLE IF EXISTS `client`;
-CREATE TABLE IF NOT EXISTS `client` (
-  `id` varchar(255) NOT NULL,
-  `nom` varchar(255) NOT NULL,
-  `prenom` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `tel` varchar(255) NOT NULL,
-  `adresse` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `coach`
---
-
-DROP TABLE IF EXISTS `coach`;
-CREATE TABLE IF NOT EXISTS `coach` (
-  `id` varchar(255) NOT NULL,
-  `nom` varchar(255) NOT NULL,
-  `prenom` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `tel` varchar(255) NOT NULL,
-  `salaire` varchar(255) NOT NULL,
-  `specialite` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `commentaire`
 --
 
 DROP TABLE IF EXISTS `commentaire`;
 CREATE TABLE IF NOT EXISTS `commentaire` (
   `id` int(16) NOT NULL,
-  `id_client` varchar(255) NOT NULL,
-  `id_coach` varchar(255) NOT NULL,
+  `id_user` varchar(255) NOT NULL,
   `id_article` int(16) NOT NULL,
   `commentaire` longtext NOT NULL,
   `date` date NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `com1` (`id_client`),
-  KEY `com2` (`id_coach`)
+  KEY `com1` (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -173,13 +119,11 @@ DROP TABLE IF EXISTS `contact`;
 CREATE TABLE IF NOT EXISTS `contact` (
   `id` int(16) NOT NULL,
   `type` varchar(255) NOT NULL,
-  `id_client` varchar(255) NOT NULL,
-  `id_coach` varchar(255) NOT NULL,
+  `id_user` varchar(255) NOT NULL,
   `champ` text NOT NULL,
   `date` date NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `con1` (`id_client`),
-  KEY `con2` (`id_coach`)
+  KEY `con1` (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -193,13 +137,11 @@ CREATE TABLE IF NOT EXISTS `entraide` (
   `id` int(16) NOT NULL,
   `question` text NOT NULL,
   `categorie` varchar(255) NOT NULL,
-  `id_coach` varchar(255) NOT NULL,
-  `id_client` varchar(255) NOT NULL,
+  `id_user` varchar(255) NOT NULL,
   `date` date NOT NULL,
   `visibilite` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `ent1` (`id_client`),
-  KEY `ent2` (`id_coach`)
+  KEY `ent1` (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -211,13 +153,13 @@ CREATE TABLE IF NOT EXISTS `entraide` (
 DROP TABLE IF EXISTS `ligne_classement`;
 CREATE TABLE IF NOT EXISTS `ligne_classement` (
   `id` int(16) NOT NULL,
-  `id_client` int(16) NOT NULL,
+  `id_client` varchar(255) NOT NULL,
   `id_niveau` int(16) NOT NULL,
   `position` int(10) NOT NULL,
   `nb_points` int(10) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `id_client` (`id_client`),
-  KEY `id_niveau` (`id_niveau`)
+  KEY `id_niveau` (`id_niveau`),
+  KEY `id_client` (`id_client`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -236,6 +178,161 @@ CREATE TABLE IF NOT EXISTS `niveau` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `objectif`
+--
+
+DROP TABLE IF EXISTS `objectif`;
+CREATE TABLE IF NOT EXISTS `objectif` (
+  `id` varchar(50) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `reponse` int(11) NOT NULL,
+  `dateDebut` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `duree` int(11) NOT NULL,
+  `idClient` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_objCli` (`idClient`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `objectif_pred`
+--
+
+DROP TABLE IF EXISTS `objectif_pred`;
+CREATE TABLE IF NOT EXISTS `objectif_pred` (
+  `id` varchar(50) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `reponse` int(11) NOT NULL,
+  `duree` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `participationactivté`
+--
+
+DROP TABLE IF EXISTS `participationactivté`;
+CREATE TABLE IF NOT EXISTS `participationactivté` (
+  `id` int(16) NOT NULL AUTO_INCREMENT,
+  `id_client` varchar(255) NOT NULL,
+  `id_activite` int(11) NOT NULL,
+  `rating` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `part1` (`id_client`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `participationactivté`
+--
+
+INSERT INTO `participationactivté` (`id`, `id_client`, `id_activite`, `rating`) VALUES
+(1, '10', 156, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `participationtherapie`
+--
+
+DROP TABLE IF EXISTS `participationtherapie`;
+CREATE TABLE IF NOT EXISTS `participationtherapie` (
+  `id` int(16) NOT NULL AUTO_INCREMENT,
+  `id_client` varchar(255) NOT NULL,
+  `id_therapie` int(11) NOT NULL,
+  `rating` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `part2` (`id_client`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `participationtherapie`
+--
+
+INSERT INTO `participationtherapie` (`id`, `id_client`, `id_therapie`, `rating`) VALUES
+(1, '10', 2020, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `participation_challenge`
+--
+
+DROP TABLE IF EXISTS `participation_challenge`;
+CREATE TABLE IF NOT EXISTS `participation_challenge` (
+  `id` int(16) NOT NULL AUTO_INCREMENT,
+  `id_challenge` int(16) NOT NULL,
+  `id_client` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_challenge` (`id_challenge`),
+  KEY `id_client` (`id_client`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `propoact`
+--
+
+DROP TABLE IF EXISTS `propoact`;
+CREATE TABLE IF NOT EXISTS `propoact` (
+  `id` int(11) NOT NULL,
+  `idcoach` varchar(255) NOT NULL,
+  `duree` varchar(255) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `nombremax` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `lieu` varchar(255) NOT NULL,
+  `nombre_parti` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `proptherapie`
+--
+
+DROP TABLE IF EXISTS `proptherapie`;
+CREATE TABLE IF NOT EXISTS `proptherapie` (
+  `id` int(11) NOT NULL,
+  `sujet` varchar(255) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `lieu` varchar(255) NOT NULL,
+  `nombremax` int(11) NOT NULL,
+  `idcoach` varchar(255) NOT NULL,
+  `nombre_parti` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `proptherapie`
+--
+
+INSERT INTO `proptherapie` (`id`, `sujet`, `date`, `lieu`, `nombremax`, `idcoach`, `nombre_parti`) VALUES
+(120, 'anixi', '24/03/2021', 'mourouj 6', 0, '1', 0),
+(232, '32', '03/03/2021', '3', 0, '12', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reminder`
+--
+
+DROP TABLE IF EXISTS `reminder`;
+CREATE TABLE IF NOT EXISTS `reminder` (
+  `id` int(16) NOT NULL AUTO_INCREMENT,
+  `id_challenge` int(16) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_challenge` (`id_challenge`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `reponse`
 --
 
@@ -243,13 +340,29 @@ DROP TABLE IF EXISTS `reponse`;
 CREATE TABLE IF NOT EXISTS `reponse` (
   `id` int(16) NOT NULL,
   `id_question` int(11) NOT NULL,
-  `id_coach` varchar(255) NOT NULL,
-  `id_client` varchar(255) NOT NULL,
+  `id_user` varchar(255) NOT NULL,
   `date` date NOT NULL,
   `reponse` text NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `rep1` (`id_coach`),
-  KEY `rep2` (`id_client`)
+  KEY `rep1` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `suivi`
+--
+
+DROP TABLE IF EXISTS `suivi`;
+CREATE TABLE IF NOT EXISTS `suivi` (
+  `id` varchar(11) NOT NULL,
+  `valeur` int(11) NOT NULL,
+  `idClient` varchar(50) NOT NULL,
+  `idObjectif` varchar(50) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `fk_SuivObj` (`idObjectif`),
+  KEY `fk_SuivCli` (`idClient`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -262,13 +375,58 @@ DROP TABLE IF EXISTS `therapie`;
 CREATE TABLE IF NOT EXISTS `therapie` (
   `id` int(16) NOT NULL,
   `sujet` varchar(254) NOT NULL,
-  `date` date NOT NULL,
+  `date` varchar(255) NOT NULL,
   `lieu` varchar(254) NOT NULL,
   `nombremax` int(11) NOT NULL,
   `idcoach` varchar(254) NOT NULL,
+  `nombre_parti` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `th1` (`idcoach`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `therapie`
+--
+
+INSERT INTO `therapie` (`id`, `sujet`, `date`, `lieu`, `nombremax`, `idcoach`, `nombre_parti`) VALUES
+(2020, '20', '24/03/2021', '20', 20, '12', 2),
+(4546, '5', '24/03/2021', '15', 20, '12', 0),
+(1212566, '12', '24/03/2021', '1', 0, '12', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `tel` varchar(255) NOT NULL,
+  `specialite` varchar(255) NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `nom`, `prenom`, `email`, `password`, `tel`, `specialite`, `adresse`, `role`) VALUES
+('10', 'client', 'clienyt', 'client@client.clienb', 'client', '26262626', '', '', 'Client'),
+('12', 'helmi', 'ahlem', 'mohamed.medimegh@esprit.tn', '123', '25616874', 'jump', 'ariana', 'CoachV'),
+('12345670', 'seif', 'Ben Salah', 'seifeddine.bensalah@hotmail.com', 'afc677037be3d92324fa6597d6c1506b534e306b', '23123123', 'test', ' ', 'SAdmin'),
+('12345671', 'sefi', 'mdptest', 'seifeddine.bensalah@gmail.com', 'afc677037be3d92324fa6597d6c1506b534e306b', '23232321', '', 'test ', 'Client'),
+('12345672', 'seif', 'mdptest', 'seifeddine.bensalah@esprit.fr', 'afc677037be3d92324fa6597d6c1506b534e306b', '95223112', 'test', ' ', 'CoachV'),
+('12345673', 'aa', 'aa', 'seifeddine.bensalah@esprit.tn', 'afc677037be3d92324fa6597d6c1506b534e306b', '23123126', '', 'aouina', 'Client'),
+('12345674', 'ss', 'ss', 'ss@gm.com', 'afc677037be3d92324fa6597d6c1506b534e306b', '03232323', 'test', ' ', 'CoachNV'),
+('12345675', 'seif', 'sha-1', 'seif@yahoo.fr', 'afc677037be3d92324fa6597d6c1506b534e306b', '23123127', 'test', ' ', 'CoachNV'),
+('12345679', 'seif', 'admin', 'seifeddine.bensalah@yahoo.com', 'afc677037be3d92324fa6597d6c1506b534e306b', '23123124', '', ' ', 'Admin');
 
 --
 -- Contraintes pour les tables déchargées
@@ -278,14 +436,13 @@ CREATE TABLE IF NOT EXISTS `therapie` (
 -- Contraintes pour la table `activite`
 --
 ALTER TABLE `activite`
-  ADD CONSTRAINT `act1` FOREIGN KEY (`idcoach`) REFERENCES `coach` (`id`);
+  ADD CONSTRAINT `act1` FOREIGN KEY (`idcoach`) REFERENCES `user` (`id`);
 
 --
 -- Contraintes pour la table `article`
 --
 ALTER TABLE `article`
-  ADD CONSTRAINT `art1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id`),
-  ADD CONSTRAINT `art2` FOREIGN KEY (`id_coach`) REFERENCES `coach` (`id`);
+  ADD CONSTRAINT `art2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
 
 --
 -- Contraintes pour la table `challenge`
@@ -297,41 +454,75 @@ ALTER TABLE `challenge`
 -- Contraintes pour la table `commentaire`
 --
 ALTER TABLE `commentaire`
-  ADD CONSTRAINT `com1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id`),
-  ADD CONSTRAINT `com2` FOREIGN KEY (`id_coach`) REFERENCES `coach` (`id`);
+  ADD CONSTRAINT `com1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
 
 --
 -- Contraintes pour la table `contact`
 --
 ALTER TABLE `contact`
-  ADD CONSTRAINT `con1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id`),
-  ADD CONSTRAINT `con2` FOREIGN KEY (`id_coach`) REFERENCES `coach` (`id`);
+  ADD CONSTRAINT `con1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
 
 --
 -- Contraintes pour la table `entraide`
 --
 ALTER TABLE `entraide`
-  ADD CONSTRAINT `ent1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id`),
-  ADD CONSTRAINT `ent2` FOREIGN KEY (`id_coach`) REFERENCES `coach` (`id`);
+  ADD CONSTRAINT `ent1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
 
 --
 -- Contraintes pour la table `ligne_classement`
 --
 ALTER TABLE `ligne_classement`
+  ADD CONSTRAINT `id_client` FOREIGN KEY (`id_client`) REFERENCES `user` (`id`),
   ADD CONSTRAINT `ligne_classement_ibfk_1` FOREIGN KEY (`id_niveau`) REFERENCES `niveau` (`id`);
+
+--
+-- Contraintes pour la table `objectif`
+--
+ALTER TABLE `objectif`
+  ADD CONSTRAINT `fk_objCli` FOREIGN KEY (`idClient`) REFERENCES `user` (`id`);
+
+--
+-- Contraintes pour la table `participationactivté`
+--
+ALTER TABLE `participationactivté`
+  ADD CONSTRAINT `part1` FOREIGN KEY (`id_client`) REFERENCES `user` (`id`);
+
+--
+-- Contraintes pour la table `participationtherapie`
+--
+ALTER TABLE `participationtherapie`
+  ADD CONSTRAINT `part2` FOREIGN KEY (`id_client`) REFERENCES `user` (`id`);
+
+--
+-- Contraintes pour la table `participation_challenge`
+--
+ALTER TABLE `participation_challenge`
+  ADD CONSTRAINT `participation_challenge_ibfk_1` FOREIGN KEY (`id_challenge`) REFERENCES `challenge` (`id`),
+  ADD CONSTRAINT `participation_challenge_ibfk_2` FOREIGN KEY (`id_client`) REFERENCES `user` (`id`);
+
+--
+-- Contraintes pour la table `reminder`
+--
+ALTER TABLE `reminder`
+  ADD CONSTRAINT `reminder_ibfk_1` FOREIGN KEY (`id_challenge`) REFERENCES `challenge` (`id`);
 
 --
 -- Contraintes pour la table `reponse`
 --
 ALTER TABLE `reponse`
-  ADD CONSTRAINT `rep1` FOREIGN KEY (`id_coach`) REFERENCES `coach` (`id`),
-  ADD CONSTRAINT `rep2` FOREIGN KEY (`id_client`) REFERENCES `client` (`id`);
+  ADD CONSTRAINT `rep1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
+
+--
+-- Contraintes pour la table `suivi`
+--
+ALTER TABLE `suivi`
+  ADD CONSTRAINT `fk_SuivCli` FOREIGN KEY (`idClient`) REFERENCES `user` (`id`);
 
 --
 -- Contraintes pour la table `therapie`
 --
 ALTER TABLE `therapie`
-  ADD CONSTRAINT `th1` FOREIGN KEY (`idcoach`) REFERENCES `coach` (`id`);
+  ADD CONSTRAINT `th1` FOREIGN KEY (`idcoach`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
